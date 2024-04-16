f7110537cf5e51ea48e6e495752557f32481f7dc
kyboahene <kyeiyaw437@gmail>
2024-04-16 07:46:59 +0000

    feat: imported css for stream and datepicker
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import "@/styles/globals.css";
 import Providers from "@/modules/shared/providers";
 import { Toaster } from "@/modules/shared/toaster";
 
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import "react-datepicker/dist/react-datepicker.css";
+
 export const metadata: Metadata = {
   title: "Lets-Meet",
   description: "A video meeting application",
