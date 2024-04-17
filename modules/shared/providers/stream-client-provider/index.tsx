d904ce807a9bbc36842e6aa59df039735cc04888
kyboahene <kyeiyaw437@gmail>
2024-04-17 08:26:30 +0000

    refactor: changed loader to skeleton
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- modules/shared/providers/stream-client-provider/index.tsx
+++ modules/shared/providers/stream-client-provider/index.tsx
@@ -9,6 +9,7 @@ import Loader from "../../loader";
 
 // action
 import { tokenProvider } from "@/actions/stream.actions";
+import SkeletonLayout from "@/modules/skeletons/templates/skeleton-layout";
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
@@ -34,7 +35,7 @@ const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
     setVideoClient(client);
   }, [user, isLoaded]);
 
-  if (!videoClient) return <Loader />;
+  if (!videoClient) return <SkeletonLayout />;
 
   return <StreamVideo client={videoClient}>{children}</StreamVideo>;
 };
